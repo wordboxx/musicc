0751ab92c8fc5ba6556a081865910ef5b251b43b
publicthreat <publicthreat@proton.me>
2024-10-17 18:08:12 -0500

    Fixed PyVenv creation

--- spotdl.sh
+++ spotdl.sh
@@ -8,26 +8,28 @@ VENV="$PWD/venv"
 MUSIC_DIR="/home/$USER/Music"
 
 # Checking for Python Virtual Environment
-if [[ -d $VENV ]]; then
-  echo "Venv found @"
-  else
+if [[ ! -d $VENV ]]; then
     # Creating PyVenv if doesn't exist
     echo "Creating Venv."
     python3 -m venv venv
-    echo "Venv created @"
 fi
 
 # Using the PyVenv as a source
 source "$VENV/bin/activate"
-
-# TODO: Check installed dependencies and weigh that against what needs to be installed.
-# Then, install if needed.
+echo "Venv activated @"
+echo $VENV
 
 # User Input
 echo "Enter URL of Spotify playlist to download:"
 read url
 
+# Download the songs
 spotdl $url
+
+# Identify .mp3 files
+for f in ./*.mp3; do
+  echo "found $f"
+done
 
 # Script Finished
 exit 0

390c9c8678c29adef49df23ab625b05fd9283796
publicthreat <publicthreat@proton.me>
2024-10-18 03:41:47 -0500

    functional download and sort

--- spotdl.sh
+++ spotdl.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
-# ---
-# This script automatically downloads and sorts the spotify playlist
-# specified by the user.
 
 # Directory Shorthands
-VENV="$PWD/venv"
+CURR_DIR="$PWD"
+VENV="$CURR_DIR/venv"
 MUSIC_DIR="/home/$USER/Music"
 
-# Checking for Python Virtual Environment
+# Python Venv Management
 if [[ ! -d $VENV ]]; then
-    # Creating PyVenv if doesn't exist
-    echo "Creating Venv."
-    python3 -m venv venv
+  echo "Creating Venv."
+  python3 -m venv venv
 fi
 
-# Using the PyVenv as a source
 source "$VENV/bin/activate"
 echo "Venv activated @"
 echo $VENV
 
-# User Input
-echo "Enter URL of Spotify playlist to download:"
+# Download Songs User Inputs
+echo "Enter URL of Spotify playlist to download or "sort" to sort:"
 read url
+if [[ $url != "sort" ]]; then
+  spotdl $url
+fi
 
-# Download the songs
-spotdl $url
+# Processing Each Downloaded MP3
+for f in *.mp3; do
 
-# Identify .mp3 files
-for f in ./*.mp3; do
-  echo "found $f"
+  # extract artist from ID3v2 output
+  artist="$(id3v2 -l echo "$f" | grep TPE1 | cut -b 38-)"
+  artist_dir="$MUSIC_DIR/$artist"
+
+  # make a directory for the artist if it doesn't exist
+  if [[ ! -d $artist_dir ]]; then
+    mkdir "$artist_dir"
+  fi
+
+  # extract album name from ID3v2 output
+  album="$(id3v2 -l "$f" | grep TALB | cut -b 32-)"
+  album_dir="$artist_dir/$album"
+
+  # make a directory for the album in the artist directory if it doesn't exist
+  if [[ ! -d $album_dir ]]; then
+    mkdir "$album_dir"
+  fi
+
+  # move MP3 to album directory within artist directory
+  mv "$f" "$album_dir"
 done
 
 # Script Finished

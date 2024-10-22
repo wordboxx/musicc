9ef54658d0fba0707f8aa1e3a69c40ede5ecc5a4
publicthreat <publicthreat@proton.me>
2024-10-22 17:58:43 -0500

    prefix identifiction and alteration

--- prefixRemover.sh
+++ prefixRemover.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Arrays
+# File Arrays
 FILES=(*)    # Array of all files in directory
 MP3_FILES=() # Empty array to add mp3's
 
-# Loop through array for mp3's
+# Loop through Files array for mp3's
 # TODO: change .sh to mp3 for final version
 for file in ${FILES[@]}; do
 	if [[ $file == *.sh ]]; then
@@ -25,13 +25,23 @@ PREFIX_TO_REMOVE=$@
 
 # Loop through base filename array
 for i in $(seq 0 ${#BASE_FILENAME_ARR[@]}); do
-	# If the spot in the arrays match, then add to target prefix
+	# If the spot in the arrays match ...
 	if [[ ${BASE_FILENAME_ARR[i]} == ${REFERENCE_FILENAME_ARR[i]} ]]; then
-		echo ${BASE_FILENAME_ARR[i]} "matches" ${REFERENCE_FILENAME_ARR[i]}
+		# ... then add to target prefix
 		PREFIX_TO_REMOVE+=${BASE_FILENAME_ARR[i]}
-	# If the letters do not match, finalize the prefix
+	# If the letters do not match,
+	# that means the prefix is complete and we can break
 	else
-		echo ${BASE_FILENAME_ARR[i]} "not matching" ${REFERENCE_FILENAME_ARR[i]}
-		echo ${PREFIX_TO_REMOVE[@]}
+		break
 	fi
+done
+
+# Announce whole prefix to user
+echo "Prefix to remove: $PREFIX_TO_REMOVE"
+
+# Loop through mp3's ...
+for file in ${MP3_FILES[@]}; do
+	# ... and remove prefix
+	# TODO: actually change filename and not just echo
+	echo $file | sed "s/$PREFIX_TO_REMOVE/removed/"
 done
